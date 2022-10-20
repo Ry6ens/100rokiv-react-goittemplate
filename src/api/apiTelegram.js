366a1d9c4fb61d5fe561ff3a5ed21a501dc9db0f
Sergey Gulaga <ry6ens@gmail.com>
2022-10-20 18:44:55 +0300

    update app

--- src/api/apiTelegram.js
+++ src/api/apiTelegram.js
@@ -6,17 +6,11 @@ import axios from "axios";
 // };
 const TELEGRAM_TOKEN="5417174408:AAHxdouY7MroFUQFimqQzG89Od7HyWntRFc"
 const TELEGRAM_CHAT_ID="-1001801505993"
-// const WEBHOOK_URL = "https://ry6ens.github.io/"
 
 const URI_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
 
 export const axiosTelegram = async (formData) => {
   const { name, tel, sum, date } = formData;
-
-//   const init = async () => {
-//     const res = await axios.get(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/setWebhook?url=${WEBHOOK_URL}`)
-//     console.log(res.data)
-// }
 
   const { data } = await axios.post(URI_API, {
     chat_id: TELEGRAM_CHAT_ID,
