6b1134452d7ec629389484045a0ed6ae9a75bc97
Sergey Gulaga <ry6ens@gmail.com>
2022-10-30 11:12:10 +0200

    update app

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,8 @@
-import { Helmet } from 'react-helmet-async';
-
 import Home from 'components/Home/Home';
 
 const HomePage = () => {
   return (
     <main>
-      <Helmet>
-        <title>100 років тому вперед</title>
-        <meta
-          name="description"
-          content="Незвичайний Ресторан в Києві Де є Традиційні Страви Української Кухні"
-        />
-      </Helmet>
       <Home />
     </main>
   );
