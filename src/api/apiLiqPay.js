93c92d9c4718303ab11f4d4813cf5cf38c31164e
Sergey Gulaga <ry6ens@gmail.com>
2022-11-01 21:16:50 +0200

    update app

--- src/api/apiLiqPay.js
+++ src/api/apiLiqPay.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  // baseURL: 'https://storokiv-server.herokuapp.com/',
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://storokiv-server.herokuapp.com/',
+  // baseURL: 'http://localhost:4000',
 });
 
 export const axiosLiqPay = async (formData) => {
