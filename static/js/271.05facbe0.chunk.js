c877e81eb6e88d18a47d1639c865ff9cb1c73ccd
Ry6ens <ry6ens@gmail.com>
2022-11-04 17:02:47 +0000

    Deploying to gh-pages from @ Ry6ens/100rokiv-react-goittemplate@d58a7225c165e68cfe2625f04b45a57a2b28bfe1 🚀

--- static/js/271.05facbe0.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_100rokiv_react_goittemplate=self.webpackChunk_100rokiv_react_goittemplate||[]).push([[271],{8271:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)("p",{children:"213123"}),(0,n.jsx)("p",{children:"213123"}),(0,n.jsx)("p",{children:"213123"})]})}}}]);
-//# sourceMappingURL=271.05facbe0.chunk.js.map
